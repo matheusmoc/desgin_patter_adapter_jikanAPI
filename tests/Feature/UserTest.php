ac2085c1cab21c1067da09b3478f0cf95a04217b
Matheus Andrade <matheusandrade009@gmail.com>
2023-02-11 23:20:36 -0300

    Feat: PHPunit Test resgister

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,15 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
+use Illuminate\Support\Str;
 
 class UserTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_an_action_that_requires_authentication()
+    public function test_an_action_that_requires_authentication_to_access_api()
     {
         $user = User::factory()->create();
 
@@ -18,5 +21,17 @@ class UserTest extends TestCase
                          ->getJson('/api/animes');
 
         $response->assertStatus(200);
+    }
+
+    public function test_if_user_register_with_all_data_requested(){
+        $response = $this->postJson('/api/signup', [
+            'name' => 'Matheus',
+            'email' => 'test@gmail.com',
+            'password' => 'testest@123', // password
+            'password_confirmation' => 'testest@123'
+        ]);
+
+        $response
+            ->assertStatus(200);
     }
 }
